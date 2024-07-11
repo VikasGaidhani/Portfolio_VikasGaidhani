497ac5b86bf5b13a290c8a1b916be5ebae447008
VikasGaidhani <vikasgaidhani@gmail.com>
2024-07-11 14:52:42 +0530

    Update contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,11 +11,15 @@ const Contact = () => {
         <p className="mb-6">
           If you have an exciting project in mind, want to collaborate, or simply wish to connect, let's initiate a conversation. 
         </p>
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center mb-4">
           <a href="mailto:vikasgaidhani001@gmail.com" className="mr-4">
-            <FontAwesomeIcon icon={faEnvelope} size="lg" className='hover:text-black'/>
+            <FontAwesomeIcon icon={faEnvelope} size="lg" className="hover:text-black" />
+          </a>
+          <a href="tel:+917218265626">
+            <FontAwesomeIcon icon={faPhone} size="lg" className="hover:text-black" />
           </a>
         </div>
+        <p className="text-lg">Call me at <a href="tel:+917218265626" className="text-black font-semibold">+91 7218265626</a></p>
       </div>
     </section>
   );
